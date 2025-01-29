08003c4f82395ba0b0f8810bc74c14bebc69bfc1
TARIQE <tariqe2.rylance@live.uwe.ac.uk>
2025-01-29 16:20:45 +0000

    FMOD Build Problem Fixed
    The game can now be built and ran successfully.

---
+++ Source/SecondChance/FmodPleaseWork.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SECONDCHANCE_API FmodPleaseWork
+{
+public:
+	FmodPleaseWork();
+	~FmodPleaseWork();
+};
